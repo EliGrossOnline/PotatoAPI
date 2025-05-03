adf7fb3f12b84a382f418f8d817047517a9f2c90
Eli Gross <eligrossonline@pm.me>
2025-05-03 14:13:56 -0500

    feat: add potato controller

---
+++ src/main/kotlin/com/eli/potatoapi/controller/PotatoController.kt
@@ -0,0 +1,14 @@
+package com.eli.potatoapi.controller
+
+import com.eli.potatoapi.model.Potato
+import com.eli.potatoapi.repository.PotatoRepository
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RequestMapping
+@RestController("/api/potatoes")
+class PotatoController(private val potatoRepository: PotatoRepository) {
+
+    @GetMapping
+    fun getAllPotatoes(): List<Potato> = potatoRepository.findAll()}
