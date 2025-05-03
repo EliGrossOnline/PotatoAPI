91d4e9bd02f1d929e810454ccab565eb56201b4f
Eli Gross <eligrossonline@pm.me>
2025-05-03 15:34:18 -0500

    Improved getPotatoByID, added error handler.

--- src/main/kotlin/com/eli/potatoapi/controller/PotatoController.kt
+++ src/main/kotlin/com/eli/potatoapi/controller/PotatoController.kt
@@ -2,9 +2,12 @@ package com.eli.potatoapi.controller
 
 import com.eli.potatoapi.model.Potato
 import com.eli.potatoapi.repository.PotatoRepository
+import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.server.ResponseStatusException
 
 @RequestMapping("/api/potatoes")
 @RestController
@@ -12,4 +15,12 @@ class PotatoController(private val potatoRepository: PotatoRepository) {
 
     @GetMapping
     fun getAllPotatoes(): List<Potato> = potatoRepository.findAll()
+
+    @GetMapping("/{id}")
+    fun getPotatoByID(@PathVariable id: Int): Potato {
+        return potatoRepository
+            .findById(id)
+            .orElseThrow{ResponseStatusException(HttpStatus.NOT_FOUND, "Potato not found!") }
+    }
+
 }
